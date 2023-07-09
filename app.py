b060ee401254e84137764bc1208a6404cec43e97
jusonja <139030083+jusonja@users.noreply.github.com>
2023-07-09 00:35:36 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Route for the second page
+@app.route('/database')
+def database():
+    # Retrieve your database data here and pass it to the template
+    # Example data
+    database_data = [
+        {'datetime': 'datadb'},
+        {'temperature': 'tempdb'},
+        {'soilhumidity': 'shdb'},
+        {'ambienthumidity': 'ahdb'}
+    ]
+    return render_template('database.html', data=database_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
